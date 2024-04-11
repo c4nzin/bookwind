8b643abdc78df00e96b5f4026997bbd85126741c
canmertinyo <38213551+c4nzin@users.noreply.github.com>
2024-04-11 18:27:04 +0300

    fix(api): add missing mongoose modules

--- apps/api/src/domain/user/user.module.ts
+++ apps/api/src/domain/user/user.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserRepository } from './repositories';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserModel } from './entities/user.schema';
+import { Follow, FollowModel } from './entities/follow.schema';
+import { Post, PostModel } from './entities/post.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserModel }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserModel },
+      { name: Follow.name, schema: FollowModel },
+      { name: Post.name, schema: PostModel },
+    ]),
   ],
   providers: [UserRepository],
   exports: [UserRepository],
