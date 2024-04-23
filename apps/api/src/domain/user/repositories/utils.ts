2b0f9b1f2d57357bbf556454bda16791c8dc74c8
canmertinyo <38213551+c4nzin@users.noreply.github.com>
2024-04-23 18:01:12 +0300

    refactor(api): add comments to urlWrapper function

--- apps/api/src/domain/user/repositories/utils.ts
+++ apps/api/src/domain/user/repositories/utils.ts
@@ -1,7 +1,11 @@
 import { BadRequestException } from '@nestjs/common';
 import { PATHS } from './user.repository';
-
-export function urlWrapper(url: string) {
+/**
+ * Wraps the URL based on certain conditions.
+ * @param url The URL to be wrapped.
+ * @returns Wrapped URL based on conditions.
+ */
+export function urlWrapper(url: string): string {
   if (url.includes(PATHS.FOLLOWERS)) {
     const followersIndex = url.indexOf(PATHS.FOLLOWERS);
     return url.slice(followersIndex);
