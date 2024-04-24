9dd3e9a8614ac058859fcc2461e6149ccb472baa
canmertinyo <38213551+c4nzin@users.noreply.github.com>
2024-04-24 10:40:00 +0300

    refactor(api): improve code quality on urlWrapper

--- apps/api/src/domain/user/repositories/utils.ts
+++ apps/api/src/domain/user/repositories/utils.ts
@@ -1,15 +1,21 @@
-import { BadRequestException } from '@nestjs/common';
 import { PATHS } from './user.repository';
+/* prettier-ignore */
+
 /**
  * Wraps the URL based on certain conditions.
  * @param url The URL to be wrapped.
  * @returns Wrapped URL based on conditions.
  */
 export function urlWrapper(url: string): string {
-  if (url.includes(PATHS.FOLLOWERS)) {
-    const followersIndex = url.indexOf(PATHS.FOLLOWERS);
-    return url.slice(followersIndex);
-  } else {
-    return PATHS.FOLLOWINGS;
-  }
+  const followersIndex = url.indexOf(PATHS.FOLLOWERS);
+  return url.includes(PATHS.FOLLOWERS) ? url.slice(followersIndex) : PATHS.FOLLOWINGS;
 }
+
+// export function urlWrapper(url: string): string {
+//   if (url.includes(PATHS.FOLLOWERS)) {
+//     const followersIndex = url.indexOf(PATHS.FOLLOWERS);
+//     return url.slice(followersIndex);
+//   } else {
+//     return PATHS.FOLLOWINGS;
+//   }
+// }
